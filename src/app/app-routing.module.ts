7aef511a6c935c2c69561ab836f8776617beb45b
noorjallad99 <48832632+noorjallad99@users.noreply.github.com>
2021-11-22 19:07:43 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RegisterComponent } from './register/register.component';
+import { TvshowsComponent } from './tvshows/tvshows.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:'register',pathMatch:'ful'},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'home',component:HomeComponent},
+  {path:'movies',component:MoviesComponent},
+  {path:'tvshows',component:TvshowsComponent},
+  {path:'**',component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesDetailsComponent } from './movies-details/movies-details.component';
+import { AboutComponent } from './about/about.component';
+import { TvshowsComponent } from './tvshows/tvshows.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NotfoundComponent,
+    MoviesComponent,
+    MoviesDetailsComponent,
+    AboutComponent,
+    TvshowsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
